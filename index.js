2378579793f44cc748b32e0cb9593b41da1093a5
Emerson Mellado <emersonmellado@gmail.com>
2020-03-27 19:12:30 -0700

    Adding admin route

--- index.js
+++ index.js
@@ -9,8 +9,10 @@ app.use(express.static('public'))
 //Modules
 const home = require('./routes/home')
 const categories = require('./routes/categories')
+const admin = require('./routes/admin')
 
 app.get('/', home)
 app.get('/categories', categories)
+app.get('/admin', admin)
 
 app.listen(port, () => console.log(`Shopping cart running on port ${port}!`))
