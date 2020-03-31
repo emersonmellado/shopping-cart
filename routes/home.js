7a53e4cd7c548e281b6b8d2ab0742ca113eb7aa9
Emerson Mellado <emersonmellado@gmail.com>
2020-03-30 19:13:14 -0700

    Replacing hard coded values with database call

--- routes/home.js
+++ routes/home.js
@@ -1,32 +1,7 @@
-const home = (req, res) => {
+const Product = require('../models/product');
 
-    const products = [
-        {
-            id: 1,
-            name: "Fender",
-            description: "American Acoustasonic Telecaster w/Bag - Sunburst",
-            price: 2599.99,
-            rating: 5,
-            image: "//www.long-mcquade.com/files/125818/md_b888772699f992d02d5a6f656eb46c1b."
-        },
-        {
-            id: 2,
-            name: "Paul Reed",
-            description: "American Acoustasonic Telecaster",
-            price: 599.99,
-            rating: 5,
-            image: "https://www.long-mcquade.com/files/161304/md_fe05943667f2b2dc311ca7b1fde071cd."
-        },
-        {
-            id: 3,
-            name: "Yamaha",
-            description: "Telecaster w/Bag - Sunburst",
-            price: 8599.99,
-            rating: 5,
-            image: "https://www.long-mcquade.com/files/172732/md_91eec7bf950a75385a51179663703ad9."
-        }
-    ]
-
+const home = async (req, res) => {
+    const products = await Product.find().exec();
     res.render('home', { products })
 }
 
